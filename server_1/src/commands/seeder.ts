da17f71a8a5a1cbfbea1d5363a4b00195e834c78
chrissetyawan <christsetyawan82@gmail.com>
2021-02-20 11:51:36 +0700

    fix

--- server_1/src/commands/seeder.ts
+++ server_1/src/commands/seeder.ts
@@ -4,24 +4,34 @@ import Team from "../models/team.model";
 
 async function seeder() {
 
-  await Role.bulkCreate([
-    {name: "Leader"},
-    {name: "Member"},
-  ]);
-  
-  await Team.bulkCreate([
-    {name: "Eagle Team"}
-  ]);
-  
-  await User.bulkCreate([
-    {roleId: 1, teamId: 1, email:"nicole@gmail.com", username: "nicole", fullname: "Nicole"},
-    {roleId: 2, teamId: 1, email:"irene@gmail.com", username: "irene", fullname: "Irene"},
-    {roleId: 2, teamId: 1, email:"michael@gmail.com", username: "michael", fullname: "Michael"},
-    {roleId: 2, teamId: 1, email:"dan@gmail.com", username: "dan", fullname: "Dan"},
-    {roleId: 2, teamId: 1, email:"kal@gmail.com", username: "kal", fullname: "Kal"},
-  ]);
+  try
+  {
+    await Role.bulkCreate([
+      {name: "Leader"},
+      {name: "Member"},
+    ]);
+    
+    await Team.bulkCreate([
+      {name: "Eagle Team"}
+    ]);
+    
+    await User.bulkCreate([
+      {roleId: 1, teamId: 1, email:"nicole@gmail.com", username: "nicole", fullname: "Nicole"},
+      {roleId: 2, teamId: 1, email:"irene@gmail.com", username: "irene", fullname: "Irene"},
+      {roleId: 2, teamId: 1, email:"michael@gmail.com", username: "michael", fullname: "Michael"},
+      {roleId: 2, teamId: 1, email:"dan@gmail.com", username: "dan", fullname: "Dan"},
+      {roleId: 2, teamId: 1, email:"kal@gmail.com", username: "kal", fullname: "Kal"},
+    ]);
 
-  console.log("💡 Database: Migration successfully executed.")
+    console.log("💡 Database: Migration successfully executed.")
+
+  }
+  catch (e)
+  {
+    
+    console.log("🔥 Error while seedering ", e)
+  }
+
 
 }
 
